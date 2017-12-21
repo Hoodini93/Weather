6d57bd3554c74eb94ce9493e4591e07f65437c11
Hoodini93 <mahersean1993@gmail.com>
2017-12-21 12:46:17 -0600

    Reformatted and placed functions into main module

--- weather.py
+++ weather.py
@@ -1,18 +1,24 @@
-import requests
-import json
+"""weather.py"""
 from pprint import pprint
+import requests
 
-degree_sign = u'\N{DEGREE SIGN}'
+def main():
+    """Function that grabs weather data"""
+    degree_sign = u'\N{DEGREE SIGN}'
+    payload = {
+        'zip': '63304,us',
+        'units': 'imperial',
+        'APPID': '05db8a6a0fbf5f2b42654061645412f5'
+        }
+    # This is grabbing OpenWeatherMap API data
+    api_data = requests.get('http://api.openweathermap.org/data/2.5/weather', params=payload)
+    weather_values = api_data.json() #Get API request placed into json format
+    #Weather values filtered on current temp and set to whole number
+    current_temp = int(weather_values['main']['temp'])
+    city = weather_values['name'] #City name pulled from weather values
 
+    print(city)
+    print('{}{} F'.format(current_temp, degree_sign))
 
-#headers = {'APPID': '05db8a6a0fbf5f2b42654061645412f5'}
-
-# This is grabbing OWM API data
-api_data = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=63304,us&units=imperial&APPID=05db8a6a0fbf5f2b42654061645412f5')
-weather_values = api_data.json() #Get API request placed into json format
-current_temp = int(weather_values['main']['temp']) #Weather values filtered on current temp and set to whole number
-city = weather_values['name'] #City name pulled from weather values
-
-print(city)
-print(current_temp, degree_sign, 'F')
-
+if __name__ == '__main__':
+    main()
